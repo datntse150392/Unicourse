e97dfffd0e36e9edb842dcadd73b6913cab5bb40
Nguyen Huy Khai <nguyenhuykhaipch94@gmail.com>
2024-01-16 00:42:12 +0700

    :art: Update & Config
    - Update Detail Page
    - Config Tailwind CSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    color: {
+      buttonWarning: 'f1b300'
+    },
+    text: {
+      customColor: '#1F1C14'
+    }
+  },
+  plugins: [],
+}
