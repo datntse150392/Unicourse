0568059161a9698181c976fefbbf84cb0bcbf14c
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-21 21:59:57 +0700

    💄Update CSS for landing page, config max-width screen

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,24 +4,24 @@ module.exports = {
   theme: {
     extend: {},
     color: {
-      buttonWarning: 'f1b300'
+      buttonWarning: "f1b300",
     },
     text: {
-      customColor: '#1F1C14'
+      customColor: "#1F1C14",
     },
     fontFamily: {
-      customFont: ['system-ui', 'Roboto', 'Helvetica', 'Arial', 'sans-serif']
+      customFont: ["system-ui", "Roboto", "Helvetica", "Arial", "sans-serif"],
     },
     screens: {
-      'customize1': {'min': '1287px', 'max': '1399px'},
-      // => @media (min-width: 1287px and max-width: 1399px) { ... }
+      // customize1: { min: "1287px", max: "1399px" },
+      // // => @media (min-width: 1287px and max-width: 1399px) { ... }
 
-      'customize2': {'min': '1400px', 'max': '1499px'},
-      // => @media (min-width: 1400px and max-width: 1499px) { ... }
+      // customize2: { min: "1400px", max: "1499px" },
+      // // => @media (min-width: 1400px and max-width: 1499px) { ... }
 
-      'customize3': {'min': '1500px'},
+      customize3: { min: "1500px" },
       // => @media (min-width: 1500px) { ... }
-    }
+    },
   },
   plugins: [],
-}
+};
