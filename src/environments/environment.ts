a95427fdba32b054a51acb922c34f5016e3a94bf
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-25 16:21:29 +0700

    change baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   // Setting environment deploy local
   production: false,
   // baseUrl: 'http://localhost:4040',
-  baseUrl: 'unicourse-v2.us-east-1.elasticbeanstalk.com',
+  baseUrl: 'http://unicourse-v3.us-east-1.elasticbeanstalk.com',
 
   // Logo
   LOGO: 'https://firebasestorage.googleapis.com/v0/b/ongbutdicode.appspot.com/o/README%2FLogo%20UniCourse.svg?alt=media&token=d05b64f7-5f30-49ea-a4ae-f553a5196e38',
