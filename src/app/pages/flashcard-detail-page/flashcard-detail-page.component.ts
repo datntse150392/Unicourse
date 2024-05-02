f7015c61da3ce76459f588ac7c7b76c4a774c871
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-05-02 15:26:53 +0700

    🐛Update Attribute

--- src/app/pages/flashcard-detail-page/flashcard-detail-page.component.ts
+++ src/app/pages/flashcard-detail-page/flashcard-detail-page.component.ts
@@ -3,7 +3,13 @@ import { SharedModule } from '../../shared';
 import { FooterComponent, HeaderComponent } from '../../shared/components';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 interface Flashcard {
   _id: number;
@@ -20,6 +26,7 @@ interface CurrentItem {
       _id: number;
       content: string;
       isChoiced: boolean;
+      rightAnswer: boolean;
     }
   ];
   rightAnswer: string[];
@@ -36,6 +43,7 @@ interface UserItemAnwer {
       _id: number;
       content: string;
       isChoiced: boolean;
+      rightAnswer: boolean;
     }
   ];
 }
@@ -43,17 +51,20 @@ interface UserItemAnwer {
 @Component({
   selector: 'app-flashcard-detail-page',
   standalone: true,
-  imports: [HeaderComponent ,SharedModule],
+  imports: [HeaderComponent, SharedModule],
   templateUrl: './flashcard-detail-page.component.html',
   styleUrl: './flashcard-detail-page.component.scss',
   animations: [
     trigger('fadeInOut', [
-      state('void', style({
-        opacity: 0
-      })),
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
       transition('void <=> *', animate(500)),
     ]),
-  ]
+  ],
 })
 export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
   originFlashcard: Flashcard = {} as Flashcard;
@@ -65,7 +76,7 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
   currentItems: CurrentItem = {} as CurrentItem;
 
   userflashcardAnwers: UserflashcardAnwers = {
-    userItemAnwer: new Map<number, UserItemAnwer>()
+    userItemAnwer: new Map<number, UserItemAnwer>(),
   } as UserflashcardAnwers;
 
   isActive: boolean[] = [];
@@ -75,10 +86,7 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
 
   private subscriptions: Subscription[] = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     // Thiết lập title cho trang
     window.document.title = 'Chi tiết flashcard';
     // Scroll smooth to top lên đầu trang
@@ -102,194 +110,203 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
           answer: [
             {
               _id: 1,
-              content: 'Our target audience is so overwhelmed with much information, so you can start with a filter and focus blog',
+              content:
+                'Our target audience is so overwhelmed with much information, so you can start with a filter and focus blog',
               isChoiced: false,
-              rightAnswer: true
+              rightAnswer: true,
             },
             {
               _id: 2,
-              content: 'Our target audience is very demanding, so you should be genius to impress them',
+              content:
+                'Our target audience is very demanding, so you should be genius to impress them',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 3,
-              content: 'Our target audience is very smart, so try to be smarter than them',
+              content:
+                'Our target audience is very smart, so try to be smarter than them',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 4,
-              content: 'Writing blog is too traditional, it is not relevant in social marketing',
+              content:
+                'Writing blog is too traditional, it is not relevant in social marketing',
               isChoiced: false,
-              rightAnswer: false
-            }
+              rightAnswer: false,
+            },
           ],
           rightAnswer: [
             {
               _id: 1,
-              content: 'Our target audience is so overwhelmed with much information, so you can start with a filter and focus blog',
-            }
-          ]
+              content:
+                'Our target audience is so overwhelmed with much information, so you can start with a filter and focus blog',
+            },
+          ],
         },
         {
           _id: 2,
-          question: 'Display ads, magazine ads, acquisition programs, endorsements, affiliate program, pay-per-click, banner ads are examples of',
+          question:
+            'Display ads, magazine ads, acquisition programs, endorsements, affiliate program, pay-per-click, banner ads are examples of',
           type: 'single',
           answer: [
             {
               _id: 1,
               content: 'owned media',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 2,
               content: 'paid media',
               isChoiced: false,
-              rightAnswer: true
+              rightAnswer: true,
             },
             {
               _id: 3,
               content: 'earned media',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 4,
               content: 'traditional media',
               isChoiced: false,
-              rightAnswer: false
-            }
+              rightAnswer: false,
+            },
           ],
           rightAnswer: [
             {
               _id: 2,
               content: 'paid media',
-            }
-          ]
+            },
+          ],
         },
         {
           _id: 3,
-          question: 'In Social marketing budget, there are four areas as follows, except:',
+          question:
+            'In Social marketing budget, there are four areas as follows, except:',
           type: 'single',
           answer: [
             {
               _id: 1,
               content: 'Empowering',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 2,
               content: 'Technology',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 3,
               content: 'Marketing program',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 4,
               content: 'Production',
               isChoiced: false,
-              rightAnswer: true
-            }
+              rightAnswer: true,
+            },
           ],
           rightAnswer: [
             {
               _id: 4,
               content: 'Production',
-            }
-          ]
+            },
+          ],
         },
         {
           _id: 4,
-          question: 'Which of the following are tools for Search Analytics? Check all that apply.',
+          question:
+            'Which of the following are tools for Search Analytics? Check all that apply.',
           type: 'multiple',
           answer: [
             {
               _id: 1,
               content: 'Google Trends',
               isChoiced: false,
-              rightAnswer: true
+              rightAnswer: true,
             },
             {
               _id: 2,
               content: 'Keyhole.co',
               isChoiced: false,
-              rightAnswer: true
+              rightAnswer: true,
             },
             {
               _id: 3,
               content: 'Boardreader',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 4,
               content: 'Answer the Public',
               isChoiced: false,
-              rightAnswer: true
-            }
+              rightAnswer: true,
+            },
           ],
           rightAnswer: [
             {
               _id: 1,
-              content: 'Google Trends'
+              content: 'Google Trends',
             },
             {
               _id: 2,
-              content: 'Keyhole.co'
+              content: 'Keyhole.co',
             },
             {
               _id: 4,
-              content: 'Answer the Public'
-            }
-          ]
+              content: 'Answer the Public',
+            },
+          ],
         },
         {
           _id: 5,
-          question: 'If you want to develop a great blog, you should avoid .......',
+          question:
+            'If you want to develop a great blog, you should avoid .......',
           type: 'single',
           answer: [
             {
               _id: 1,
               content: 'Using Google Suggest for finding suitable topics',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 2,
               content: 'having a great headline',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 3,
               content: 'using images',
               isChoiced: false,
-              rightAnswer: false
+              rightAnswer: false,
             },
             {
               _id: 4,
               content: 'writing a lot of long paragraphs',
               isChoiced: false,
-              rightAnswer: true
-            }
+              rightAnswer: true,
+            },
           ],
           rightAnswer: [
             {
               _id: 4,
               content: 'writing a lot of long paragraphs',
-            }
-          ]
-        }
-      ]
-    }
+            },
+          ],
+        },
+      ],
+    };
 
     this.userFlashcard = this.originFlashcard;
     this.currentIndex = 0;
@@ -300,7 +317,6 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
   toggleActive(item: any, currentItems: any) {
     this.updateUserAnswer(item, currentItems);
   }
-
 
   nextQuestion() {
     if (this.currentIndex < this.userFlashcard.items.length - 1) {
@@ -361,11 +377,9 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
         });
       }
     });
-
   }
 
-  onCalculateScore() {
-  }
+  onCalculateScore() {}
 
   resetValues() {
     this.finalScore = 0;
@@ -373,7 +387,7 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
     this.currentItems = this.userFlashcard.items[this.currentIndex];
     this.isShowRightAnswer = false;
     this.isActive = [];
-    this.currentItems.answer.forEach(_ => this.isActive.push(false));
+    this.currentItems.answer.forEach((_) => this.isActive.push(false));
   }
 
   handleToggleDisabledButton() {
@@ -390,8 +404,7 @@ export class FlashcardDetailPageComponent implements OnInit, OnDestroy {
     }
   }
 
-  finishQuiz() {
-  }
+  finishQuiz() {}
 
   onChangeMode(filter: string) {
     this.filter = filter;
