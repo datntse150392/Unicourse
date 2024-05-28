2b6cd866ee6d7c4905c90c925a29e09d6f12145c
Nguyen Thanh Dat <110538773+datntse150392@users.noreply.github.com>
2024-05-28 16:06:09 +0700

    Improve SEO

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -19,6 +19,7 @@ import { DialogBroadcastService } from '../../cores/services/dialog-broadcast.se
 import { CheckingDailyEventService } from '../../cores/services/checking-daily-event.service';
 import { switchMap } from 'rxjs/operators';
 import { PayOSService } from '../../cores/services/payOS.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing-page',
@@ -56,10 +57,45 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     private readonly coinService: CoinService,
     private readonly sharedService: SharedService,
     private readonly scheduleMeetingService: ScheduleMeetingService,
-    private readonly payOSService: PayOSService
+    private readonly payOSService: PayOSService,
+    private readonly meta: Meta,
+    private readonly titleService: Title
   ) {
     // Thiết lặp title cho trang
     window.document.title = 'Unicourse - Nền Tảng Học Tập Trực Tuyến';
+    // Cập nhật tiêu đề trang
+    this.titleService.setTitle(
+      'Unicourse - Khóa học dành riêng cho sinh viên ĐH FPT'
+    );
+
+    // Cập nhật các thẻ meta
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Unicourse cung cấp các khóa học và lộ trình học tập dành riêng cho sinh viên Đại học FPT. Tìm hiểu ngay tại unicourse.vn!',
+      },
+      {
+        name: 'keywords',
+        content:
+          'Unicourse, unicourse.vn, khóa học, sinh viên ĐH FPT, học tập, lộ trình, lộ trình, unicourse ĐH FPT',
+      },
+      { name: 'author', content: 'Unicourse Team' },
+      {
+        property: 'og:title',
+        content: 'Unicourse - Khóa học dành riêng cho sinh viên ĐH FPT',
+      },
+      {
+        property: 'og:description',
+        content:
+          'Khám phá các khóa học và lộ trình học tập tại Unicourse, dành riêng cho sinh viên ĐH FPT.',
+      },
+      {
+        property: 'og:image',
+        content:
+          'https://firebasestorage.googleapis.com/v0/b/unicourse-f4020.appspot.com/o/Baner%2FTo%CC%82%CC%89ng%20ho%CC%9B%CC%A3p%20thie%CC%82%CC%81t%20ke%CC%82%CC%81%20banner.jpg?alt=media&token=c9d4ee1a-1ecf-4fa0-a397-0a6c041bf62chttps://firebasestorage.googleapis.com/v0/b/unicourse-f4020.appspot.com/o/Baner%2F6.png?alt=media&token=1f9d38c2-782a-4578-8ccd-89386815bd0a',
+      },
+    ]);
     // Scroll smooth lên đầu trang
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
+import { Meta, Title } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -9,4 +11,32 @@ import { environment } from '../../../../environments/environment.development';
 })
 export class FooterComponent {
   public Logo = environment.LOGO_FOOTER;
+
+  constructor(private meta: Meta, private titleService: Title) {
+    // Cập nhật các thẻ meta
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Unicourse cung cấp các khóa học và lộ trình học tập dành riêng cho sinh viên Đại học FPT. Tìm hiểu thêm tại unicourse.vn!',
+      },
+      {
+        name: 'keywords',
+        content:
+          'Unicourse, unicourse.vn, khóa học, sinh viên ĐH FPT, học tập, lộ trình, unicourse ĐH FPT',
+      },
+      { name: 'author', content: 'Unicourse Team' },
+      { property: 'og:title', content: 'Unicourse - Khóa học ĐH FPT' },
+      {
+        property: 'og:description',
+        content:
+          'Khám phá các khóa học và lộ trình học tập tại Unicourse, dành riêng cho sinh viên ĐH FPT.',
+      },
+      {
+        property: 'og:image',
+        content:
+          'https://firebasestorage.googleapis.com/v0/b/unicourse-f4020.appspot.com/o/Baner%2FTo%CC%82%CC%89ng%20ho%CC%9B%CC%A3p%20thie%CC%82%CC%81t%20ke%CC%82%CC%81%20banner.jpg?alt=media&token=c9d4ee1a-1ecf-4fa0-a397-0a6c041bf62chttps://firebasestorage.googleapis.com/v0/b/unicourse-f4020.appspot.com/o/Baner%2F6.png?alt=media&token=1f9d38c2-782a-4578-8ccd-89386815bd0a',
+      },
+    ]);
+  }
 }
